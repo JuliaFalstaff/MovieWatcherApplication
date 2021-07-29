2ed63112ed86a2330d6137ee1c91a727c8ed3ecd
julia <j.kholstinina@yandex.ru>
2021-07-29 17:36:49 +0300

    add toolbar, options menu and navigation between fragments

--- app/src/main/java/com/example/myapplication/model/data/Movie.kt
+++ app/src/main/java/com/example/myapplication/model/data/Movie.kt
@@ -6,7 +6,7 @@ data class Movie(
         val originalTitle: String = "Lost in Translation",
         val genres: String = "Drama",
         val releaseDate: Int = 2004,
-        val overview: String = "Билл Мюррей и Скарлетт Йохансон в фильме Софии Копполы",
+        val overview: String = "Билл Мюррей и Скарлетт Йоханссон в фильме Софии Копполы",
         val posterPath: String = R.drawable.poster_lost_in_translation.toString()
 )
 
@@ -14,6 +14,6 @@ fun getDefaultMovie() = Movie(
         "Lost in Translation",
         "Drama",
         2004,
-        "Билл Мюррей и Скарлетт Йохансон в фильме Софии Копполы",
+        "Билл Мюррей и Скарлетт Йоханссон в фильме Софии Копполы",
         R.drawable.poster_lost_in_translation.toString()
 )
--- app/src/main/java/com/example/myapplication/view/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/view/MainActivity.kt
@@ -2,7 +2,14 @@ package com.example.myapplication.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.renderscript.ScriptGroup
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toolbar
+import androidx.fragment.app.Fragment
 import com.example.myapplication.R
+import com.example.myapplication.databinding.AppBarBinding
+import com.example.myapplication.databinding.ContentMainBinding
 import com.example.myapplication.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,8 +22,51 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                    .replace(binding.container.id, MainFragment.newInstance())
-                    .commitNow()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
         }
+        initView()
+    }
+
+    //через binding больше не видит container после преобразования xml для toolbar с меню. В чем может быть причина? Он не видит вложенные layouts?
+    private fun addFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, fragment)
+            .addToBackStack(null)
+            .commit()
+    }
+
+    private fun initView() {
+        initToolBar()
+    }
+
+    private fun initToolBar() {
+        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.action_favourite -> {
+                addFragment(FavouriteMovieFragment())
+                return true
+            }
+            R.id.action_rated -> {
+                addFragment(RatedMovieFragment())
+                return true
+            }
+            R.id.action_main -> {
+                addFragment(MainFragment())
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
+
+
